acccda22a1b3acd67c40013366176aeb93c1061a
WuRuiXuan <740461697@qq.com>
2019-07-08 11:01:13 +0800

    'build.gradle'

--- app/src/main/java/com/example/wuruixuan/androidstudy/activity/ToastActivity.java
+++ app/src/main/java/com/example/wuruixuan/androidstudy/activity/ToastActivity.java
@@ -1,10 +1,7 @@
 package com.example.wuruixuan.androidstudy.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
