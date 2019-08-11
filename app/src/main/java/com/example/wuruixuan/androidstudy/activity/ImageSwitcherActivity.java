519189b55a4e9741e353ab3c6f79ee9b696f48c6
wuruixuan <740461697@qq.com>
2019-08-11 20:56:50 +0800

    ImageSwitcher

---
+++ app/src/main/java/com/example/wuruixuan/androidstudy/activity/ImageSwitcherActivity.java
@@ -0,0 +1,59 @@
+package com.example.wuruixuan.androidstudy.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
+
+import com.example.wuruixuan.androidstudy.R;
+
+public class ImageSwitcherActivity extends AppCompatActivity implements ViewSwitcher.ViewFactory, View.OnTouchListener {
+
+    private ImageSwitcher is;
+    private int[] images = { R.mipmap.a, R.mipmap.b, R.mipmap.c, R.mipmap.d };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_switcher);
+        is = findViewById(R.id.imageSwitcher);
+        is.setFactory(this);
+        is.setOnTouchListener(this);
+    }
+
+    @Override
+    public View makeView() {
+        ImageView iv = new ImageView(this);
+        iv.setImageResource(images[0]);
+        return iv;
+    }
+
+    float startX = 0.0f;
+    float endX = 0.0f;
+    private int index;
+
+    // 触屏事件
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int action = event.getAction(); // 获取当前的事件动作
+        if (action == MotionEvent.ACTION_DOWN) {
+            startX = event.getX();
+            return true;
+        }
+        else if (action == MotionEvent.ACTION_UP) {
+            endX = event.getX();
+            if (startX - endX > 20) { // 下一张
+                index = index + 1 < images.length ? ++index : 0;
+            } else if (endX - startX > 20) { // 上一张
+                index = index - 1 < 0 ? images.length - 1 : --index;
+            }
+            is.setImageResource(images[index]);
+
+            return true;
+        }
+        return false;
+    }
+}
